85437fd25f6deffe3f5fe5cd58ac26729adaa1ae
duyixian <duyixian@meideng.net>
2019-04-23 14:35:00 +0800

    v0.1.2

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name='oneday',
-    version='0.1.1',
+    version='0.1.2',
     author='Yixian Du',
     author_email='duyixian1234@qq.com',
     classifiers=[
